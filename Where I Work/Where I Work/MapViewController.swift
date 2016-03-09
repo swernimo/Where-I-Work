2e77bdac62831615ac9bcffc78dd1d9d66c9f7c8
Sean Wernimont <53341715@opayq.com>
2016-03-08 20:55:57 -0700

    [finished #115110641] displaying user's current location on the map.

--- Where I Work/Where I Work/MapViewController.swift
+++ Where I Work/Where I Work/MapViewController.swift
@@ -9,7 +9,38 @@
 import Foundation
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController : UIViewController{
+class MapViewController : UIViewController, CLLocationManagerDelegate{
     
+    var locationManager: CLLocationManager!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+       setupLocationManager()
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let segueName = segue.identifier;
+    }
+    
+    func setupLocationManager()->Void{
+        if(CLLocationManager.locationServicesEnabled()){
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //code taken from http://stackoverflow.com/questions/25449469/swift-show-current-location-and-update-location-in-a-mkmapview
+        let location = locations.last! as CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        mapView.setRegion(region, animated: true)
+    }
 }
