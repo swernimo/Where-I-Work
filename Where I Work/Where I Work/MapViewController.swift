b60912cb65414bd323c3ae4eb1c4d1884b9c2705
Sean Wernimont <53341715@opayq.com>
2016-03-28 18:31:38 -0600

    [finish #116440867] added support for long press on the map to create a new location.

--- Where I Work/Where I Work/MapViewController.swift
+++ Where I Work/Where I Work/MapViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import MapKit
 import CoreLocation
+import AddressBook
 
 class MapViewController : UIViewController, CLLocationManagerDelegate, MKMapViewDelegate{
     
@@ -17,6 +18,8 @@ class MapViewController : UIViewController, CLLocationManagerDelegate, MKMapView
     let locationHelper = LocationHelper()
     var locationArray: [Location] = []
     var loadDataFromYelp: Bool = true
+    var longPressLat: Double? = nil
+    var longPressLong: Double? = nil
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -80,6 +83,11 @@ class MapViewController : UIViewController, CLLocationManagerDelegate, MKMapView
     func setupMapView(){
         mapView.delegate = self
         mapView.removeAnnotations(mapView.annotations)
+        let longPressRecogniser = UILongPressGestureRecognizer(target: self, action: #selector(MapViewController.handleLongPress(_:)))
+        
+        longPressRecogniser.minimumPressDuration = 1.5
+        
+        mapView.addGestureRecognizer(longPressRecogniser)
         zoomMapToCurrentLocation()
     }
     
@@ -164,9 +172,93 @@ class MapViewController : UIViewController, CLLocationManagerDelegate, MKMapView
         }
         
         if(selectedLocation != nil){
-            //print("selected location id is: \(selectedLocation!.id)")
             performSegueWithIdentifier("rateLocationSegue", sender: selectedLocation)
         }
         mapView.deselectAnnotation(view.annotation!, animated: false)
+    }
+    
+    func handleLongPress(gestureRecognizer: UILongPressGestureRecognizer) -> Void{
+        /*
+         code pulled from http://stackoverflow.com/questions/3959994/how-to-add-a-push-pin-to-a-mkmapviewios-when-touching/3960754#3960754
+         */
+        if(gestureRecognizer.state != .Began){
+            return;
+        }
+        let touchPoint = gestureRecognizer.locationInView(self.mapView)
+        let touchMapCoordinate = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = touchMapCoordinate
+        
+        longPressLat = touchMapCoordinate.latitude
+        longPressLong = touchMapCoordinate.longitude
+        
+        mapView.addAnnotation(annotation)
+        
+        let viewController = createNewLocationAlertView()
+        presentViewController(viewController, animated: true, completion: nil)
+    }
+    
+    func createNewLocationAlertView() -> UIAlertController{
+        let viewController = UIAlertController(title: "Add Location", message: nil, preferredStyle: .Alert)
+        viewController.addTextFieldWithConfigurationHandler({
+            (textField) in
+            
+            textField.placeholder = "Business Name"
+        })
+        
+        viewController.addTextFieldWithConfigurationHandler({
+            (textField) in
+            
+            textField.placeholder = "Website"
+        })
+        
+        viewController.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Category"
+        }
+        viewController.addAction(UIAlertAction(title: "Save Location", style: .Default, handler: {
+            (alert) in
+            let nameTextField = viewController.textFields![0] as UITextField
+            let websiteTextField = viewController.textFields![1] as UITextField
+            let category = viewController.textFields![2] as UITextField
+            self.createAndSaveLocation(nameTextField.text!, website: websiteTextField.text, category: category.text!)
+        }))
+        
+        return viewController
+    }
+    
+    func createAndSaveLocation(name: String, website: String?, category: String) -> Void{
+        let context = CoreDataStackManager.sharedInstance().managedObjectContext
+        
+        let geoCoder = CLGeocoder()
+        let clLocation = CLLocation(latitude: longPressLat!, longitude: longPressLong!)
+        
+        geoCoder.reverseGeocodeLocation(clLocation, completionHandler: {
+            (placemarks, error) in
+            
+            if(placemarks == nil || error != nil){
+                return
+            }
+            
+            let placemark = placemarks?.first
+            
+            let dictionary = placemark!.addressDictionary!
+            
+            let street = dictionary[kABPersonAddressStreetKey]!.description
+            let city = dictionary[kABPersonAddressCityKey]!.description
+            let state = dictionary[kABPersonAddressStateKey]!.description
+            let zip = dictionary[kABPersonAddressZIPKey]!.description
+            
+            let address = Address(street: street, city: city, zip: zip, state: state, context: context)
+            
+            let id = NSUUID().UUIDString
+            let location = Location(id: id, lat: self.longPressLat!, long: self.longPressLong!, name: name, adr: address, url: website, category: category, context: context)
+            
+            CoreDataStackManager.sharedInstance().saveContext()
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                self.performSegueWithIdentifier("rateLocationSegue", sender: location)
+            })
+        })
     }
 }
