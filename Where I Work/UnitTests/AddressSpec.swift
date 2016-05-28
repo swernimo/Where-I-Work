5f918f9c70bbcc0ff88c081a64b918a7a7370ce6
Sean Wernimont <53341715@opayq.com>
2016-05-27 23:29:51 -0400

    [#116558713] continued work on unit testing. currently need to introduce mocking. tests do not run.

--- Where I Work/UnitTests/AddressSpec.swift
+++ Where I Work/UnitTests/AddressSpec.swift
@@ -10,11 +10,29 @@ class AddressSpec: QuickSpec {
         var address: Address!
         var context: NSManagedObjectContext!
         beforeEach{
-            context = NSManagedObjectContext(coder: NSCoder())
-            address = Address(street: "123 Main", city: "Any City", zip: "12345", state: "NC", context: context)
+            context = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+            
+            let entity =  NSEntityDescription.entityForName("AddressTest", inManagedObjectContext: context)!
+            address = Address(entity: entity, insertIntoManagedObjectContext: context)
+            address.street = "123 Main"
+            address.city = "Any City"
+            address.zipCode = "12345"
+            address.state = "NC"
+//            address = Address(street: "123 Main", city: "Any City", zip: "12345", state: "NC", context: context)
         }
         describe("get Address Display String"){
-            it("should return the line breaks when includeNewLine is return"){
+            it("should return the line breaks when includeNewLine is true"){
+                let expected = ""
+                let actual = address.getAddressDisplayString(true)
+                
+                expect(actual).to(be(expected))
+            }
+            
+            it("should pass"){
+                expect(true).to(beTrue())
+            }
+            
+            it("should fail"){
                 expect(false).to(beTrue())
             }
         }
---
+++ Where I Work/UnitTests/QuickConfig.swift
@@ -0,0 +1,8 @@
+import Quick
+
+class QuickConfig: QuickConfiguration {
+    override class func configure(configuration: Configuration) {
+        //see https://github.com/Quick/Quick/blob/master/Documentation/en-us/ConfiguringQuick.md for samples how to configure Quick
+    }
+}
+
